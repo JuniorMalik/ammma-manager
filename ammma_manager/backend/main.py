2f0e6732bd2a78fed5a47ad687b44d97853a5ae8
WilsonJunior <wilsonjr607@gmail.com>
2026-05-19 21:18:11 -0300

    Update main.py

--- ammma_manager/backend/main.py
+++ ammma_manager/backend/main.py
@@ -56,7 +56,7 @@ app = FastAPI(title="AMMMA 3D Manager API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
